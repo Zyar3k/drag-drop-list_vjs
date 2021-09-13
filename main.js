493072037d34b1c11a9b413afcf2eb120f586e71
Arkadiusz Zygan <a.zygan@gmail.com>
2021-09-13 12:37:30 +0200

    Insert list item into DOM

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log('init')
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const biggestCities = [
+  "Tokyo",
+  "Delhi",
+  "Shanghai",
+  "Sao Paulo",
+  "Mexico City",
+  "Cairo",
+  "Dhaka",
+  "Mumbai",
+  "Beijing",
+  "Osaka"
+];
+
+// store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// inster list items to DOM
+
+function createList() {
+  [...biggestCities].forEach((city, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.setAttribute("data-index", index);
+
+    listItem.innerHTML = `
+      <span class='number'>${index + 1}</span>
+      <div class='draggable' draggable='true'>
+        <p class='city-name'>${city}</p>
+        <i class='fas fa-grip-lines'></i>
+      </div>
+    `;
+
+    listItems.push(listItem);
+
+    draggable_list.appendChild(listItem);
+  });
+}
